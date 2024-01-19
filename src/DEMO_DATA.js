9cf0d6c6a31794b830d34c22a41af6f6cc4066d8
admin <anasszeroual09@gmail.com>
2024-01-19 14:57:16 +0100

    ADdEMO

---
+++ src/DEMO_DATA.js
@@ -0,0 +1,295 @@
+
+
+export const DEMO_DATA=[
+    {
+        matricule:3,
+        name:"ES SAKALI",
+        lastName:"MOHAMED",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:5586,
+        name:"BOUSSAKLA",
+        lastName:"RIDA",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:7200,
+        name:"LOTFI",
+        lastName:"DRISS",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:7575,
+        name:"ABDLHAK",
+        lastName:"MOUJAHID",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:6738,
+        name:"ECHACHA",
+        lastName:"TARIK",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:7866,
+        name:"LAANAYA",
+        lastName:"KHAOULA",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:5789,
+        name:"SALMI",
+        lastName:"ZINEB",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:7868,
+        name:"DIAB",
+        lastName:"AMINE",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:4927,
+        name:"HAMDNI",
+        lastName:"FATIMA",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:3792,
+        name:"SLASSI",
+        lastName:"MOHAMMED",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:627,
+        name:"SIFI",
+        lastName:"YOUSSEF",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:203,
+        name:"LAKHDAR",
+        lastName:"SOUFIAN",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:6020,
+        name:"HAMADI",
+        lastName:"FATIMA ZAHRA",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:3902,
+        name:"EL OUAFI",
+        lastName:"AYOUB",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:3302,
+        name:"LAAMECH",
+        lastName:"ZINEB",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:8029,
+        name:"LACHKER",
+        lastName:"MARYEM",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:9037,
+        name:"LAAFAFRA",
+        lastName:"KAWTAR",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:8092,
+        name:"ED-DAMGHI",
+        lastName:"LAILA",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:221,
+        name:"OUDADA",
+        lastName:"AYOUB",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:7218,
+        name:"LEGHMARI",
+        lastName:"AYOUB",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"R01B"
+    },
+    {
+        matricule:880,
+        name:"SRIFI",
+        lastName:"MOUAD",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:211,
+        name:"BOUKHDILI",
+        lastName:"OMAR",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:7778,
+        name:"NOUISSIR",
+        lastName:"BRAHIM",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:5563,
+        name:"STITOU",
+        lastName:"ANAS",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+    {
+        matricule:,
+        name:"",
+        lastName:"",
+        category:"DH",
+        plannigLeader:"fellipo inzaghi",
+        userLeader:"andrea pirlo",
+        crew:"K01A",
+        parada:""
+    },
+
+]
