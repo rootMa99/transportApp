3ad296eeac9e3dc6dcd8d15b5c7c3cb72ee79208
admin <anasszeroual09@gmail.com>
2024-01-21 22:21:52 +0100

    trk

--- src/component/planning/Planning.js
+++ src/component/planning/Planning.js
@@ -1,6 +1,12 @@
 import { useSelector } from "react-redux";
 import c from "../home/Home.module.css";
 import cs from "../home/Crews.module.css";
+import morning from "../../assets/morning.png";
+import eveningpic from "../../assets/evening.png";
+import nightPic from "../../assets/night.png";
+import enAm from "../../assets/8am.png";
+import enpm from "../../assets/16h.png";
+import enpmt from "../../assets/17h.png";
 import { getCrewsfilter, getIndivFilter } from "../hooks/getCrews";
 import { useState } from "react";
 
@@ -12,10 +18,12 @@ const Planning = (p) => {
   const [mor, setMor] = useState([]);
   const [evening, setEvening] = useState([]);
   const [type, setType] = useState();
-  console.log(crews, individuals, night, type);
-  const handleDragStart = (e, id, type) => {
+  const [zone, setZone] = useState();
+  console.log(crews, individuals, night, type, zone);
+  const handleDragStart = (e, id, type, zone) => {
     e.dataTransfer.setData("text/plain", id);
     setType(type);
+    setZone(zone);
   };
 
   const handleDragOver = (e) => {
@@ -28,63 +36,82 @@ const Planning = (p) => {
 
     // Perform the action you want with the dragged item and target
     console.log(`Item with ID ${draggedItem} dropped onto ${targetId}`);
-    if (type === "indiv") {
+    if (zone === undefined) {
+      const selected =
+        type === "indiv"
+          ? individuals.filter((f) => f.matricule === +draggedItem)
+          : crews.filter((f) => f.crewName === draggedItem);
+      console.log("ruun", selected);
       if (targetId === "night") {
-        const selected = individuals.filter(
-          (f) => f.matricule === +draggedItem
-        );
-        console.log("ruun", selected);
+        if (type === "indiv") {
+          if (selected[0].status === "inem") {
+            alert("This employee cannot work the night shift.");
+          }
+        } else {
+          selected[0].employee.map(
+            (m) =>
+              m.status === "inem" &&
+              alert(
+                `This employee with matricule ${m.matricule} fullName ${m.name} ${m.lastName} cannot work the night shift.`
+              )
+          );
+        }
         setNight((prev) => [...prev, ...selected]);
-        setIndividuals((prev) => [
-          ...prev.filter((f) => f.matricule !== +draggedItem),
-        ]);
       }
-      if (targetId === "morning") {
-        const selected = individuals.filter(
-          (f) => f.matricule === +draggedItem
-        );
-        console.log("ruun", selected);
-        setMor((prev) => [...prev, ...selected]);
-        setIndividuals((prev) => [
-          ...prev.filter((f) => f.matricule !== +draggedItem),
-        ]);
-      }
-      if (targetId === "evening") {
-        const selected = individuals.filter(
-          (f) => f.matricule === +draggedItem
-        );
-        console.log("ruun", selected);
-        setEvening((prev) => [...prev, ...selected]);
-        setIndividuals((prev) => [
-          ...prev.filter((f) => f.matricule !== +draggedItem),
-        ]);
-      }
-    }
-    if (type === "crew") {
-      if (targetId === "night") {
-        const selected = individuals.filter((f) => f.crewName === draggedItem);
-        console.log("ruun", selected);
-        setNight((prev) => [...prev, ...selected]);
-        setCrews((prev) => [
-          ...prev.filter((f) => f.crewName !== draggedItem),
-        ]);
-      }
-      if (targetId === "morning") {
-        const selected = individuals.filter((f) => f.crewName === draggedItem);
-        console.log("ruun", selected);
-        setMor((prev) => [...prev, ...selected]);
-        setCrews((prev) => [
-          ...prev.filter((f) => f.crewName !== draggedItem),
-        ]);
-      }
-      if (targetId === "evening") {
-        const selected = individuals.filter((f) => f.crewName === draggedItem);
-        console.log("ruun", selected);
-        setEvening((prev) => [...prev, ...selected]);
-        setCrews((prev) => [
-          ...prev.filter((f) => f.crewName !== draggedItem),
-        ]);
-      }
+      targetId === "morning" && setMor((prev) => [...prev, ...selected]);
+      targetId === "evening" && setEvening((prev) => [...prev, ...selected]);
+
+      type === "indiv"
+        ? setIndividuals((prev) => [
+            ...prev.filter((f) => f.matricule !== +draggedItem),
+          ])
+        : setCrews((prev) => [
+            ...prev.filter((f) => f.crewName !== draggedItem),
+          ]);
+    } else {
+      const selecte = () => {
+        if (zone === "morning") {
+          type === "indiv"
+            ? setMor((prev) => [
+                ...prev.filter((f) => f.matricule !== +draggedItem),
+              ])
+            : setMor((prev) => [
+                ...prev.filter((f) => f.crewName !== draggedItem),
+              ]);
+          return type === "indiv"
+            ? mor.filter((f) => f.matricule === +draggedItem)
+            : mor.filter((f) => f.crewName === draggedItem);
+        }
+        if (zone === "evening") {
+          type === "indiv"
+            ? setEvening((prev) => [
+                ...prev.filter((f) => f.matricule !== +draggedItem),
+              ])
+            : setEvening((prev) => [
+                ...prev.filter((f) => f.crewName !== draggedItem),
+              ]);
+          return type === "indiv"
+            ? evening.filter((f) => f.matricule === +draggedItem)
+            : evening.filter((f) => f.crewName === draggedItem);
+        }
+        if (zone === "night") {
+          type === "indiv"
+            ? setNight((prev) => [
+                ...prev.filter((f) => f.matricule !== +draggedItem),
+              ])
+            : setNight((prev) => [
+                ...prev.filter((f) => f.crewName !== draggedItem),
+              ]);
+          return type === "indiv"
+            ? night.filter((f) => f.matricule === +draggedItem)
+            : night.filter((f) => f.crewName === draggedItem);
+        }
+      };
+      const selected = selecte();
+      console.log("ruun", selected);
+      targetId === "night" && setNight((prev) => [...prev, ...selected]);
+      targetId === "morning" && setMor((prev) => [...prev, ...selected]);
+      targetId === "evening" && setEvening((prev) => [...prev, ...selected]);
     }
   };
 
@@ -126,35 +153,24 @@ const Planning = (p) => {
           </div>
         </div>
         <div className={cs.crewHolderPSh}>
+          <h1 className={cs.titles}>Shifts</h1>
           <div
-            className={`${cs.crewHolderPs} ${cs.crewHolderPsM}`}
+            className={`${cs.crewHolderPs} ${cs.crewHolderPsM} ${cs.crewHolderP}`}
             onDragOver={handleDragOver}
             onDrop={(e) => handleDrop(e, "morning")}
           >
-            <h1 className={cs.titles}>morning</h1>
-          </div>
-          <div
-            className={`${cs.crewHolderPs} ${cs.crewHolderPsE}`}
-            onDragOver={handleDragOver}
-            onDrop={(e) => handleDrop(e, "evening")}
-          >
-            <h1 className={cs.titles}>evenning</h1>
-          </div>
-          <div
-            className={`${cs.crewHolderPs} ${cs.crewHolderPsN}`}
-            onDragOver={handleDragOver}
-            onDrop={(e) => handleDrop(e, "night")}
-          >
-            <h1 className={cs.titles}>night</h1>
-            {night.length === 0 ? (
-              <h1>NO Item</h1>
+            <img src={morning} alt="mor" className={cs.img} />
+            {mor.length === 0 ? (
+              <h1 className={cs.notFound}>NO Item</h1>
             ) : (
-              night.map((m, i) =>
+              mor.map((m, i) =>
                 m.hasOwnProperty("crewName") ? (
                   <span
                     key={i}
                     draggable
-                    onDragStart={(e) => handleDragStart(e, m.crewName, "crew")}
+                    onDragStart={(e) =>
+                      handleDragStart(e, m.crewName, "crew", "morning")
+                    }
                   >
                     {m.crewName}
                   </span>
@@ -163,7 +179,7 @@ const Planning = (p) => {
                     key={i}
                     draggable
                     onDragStart={(e) =>
-                      handleDragStart(e, m.matricule, "indiv")
+                      handleDragStart(e, m.matricule, "indiv", "morning")
                     }
                   >
                     {m.matricule}
@@ -171,6 +187,96 @@ const Planning = (p) => {
                 )
               )
             )}
+          </div>
+          <div
+            className={`${cs.crewHolderPs} ${cs.crewHolderPsE} ${cs.crewHolderP}`}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, "evening")}
+          >
+            <img src={eveningpic} alt="mor" className={cs.img} />
+
+            {evening.length === 0 ? (
+              <h1 className={cs.notFound}>NO Item</h1>
+            ) : (
+              evening.map((m, i) =>
+                m.hasOwnProperty("crewName") ? (
+                  <span
+                    key={i}
+                    draggable
+                    onDragStart={(e) =>
+                      handleDragStart(e, m.crewName, "crew", "evening")
+                    }
+                  >
+                    {m.crewName}
+                  </span>
+                ) : (
+                  <span
+                    key={i}
+                    draggable
+                    onDragStart={(e) =>
+                      handleDragStart(e, m.matricule, "indiv", "evening")
+                    }
+                  >
+                    {m.matricule}
+                  </span>
+                )
+              )
+            )}
+          </div>
+          <div
+            className={`${cs.crewHolderPs} ${cs.crewHolderPsN} ${cs.crewHolderP}`}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, "night")}
+          >
+            <img src={nightPic} alt="mor" className={cs.img} />
+            {night.length === 0 ? (
+              <h1 className={cs.notFound}>NO Item</h1>
+            ) : (
+              night.map((m, i) =>
+                m.hasOwnProperty("crewName") ? (
+                  <span
+                    key={i}
+                    draggable
+                    onDragStart={(e) =>
+                      handleDragStart(e, m.crewName, "crew", "night")
+                    }
+                  >
+                    {m.crewName}
+                  </span>
+                ) : (
+                  <span
+                    key={i}
+                    draggable
+                    onDragStart={(e) =>
+                      handleDragStart(e, m.matricule, "indiv", "night")
+                    }
+                  >
+                    {m.matricule}
+                  </span>
+                )
+              )
+            )}
+          </div>
+        </div>
+        <div className={cs.crewHolderPSh}>
+          <h1 className={cs.titles}>Admin</h1>
+          <div
+            className={`${cs.crewHolderPs} ${cs.crewHolderAdmin} ${cs.crewHolderP}`}
+            // onDragOver={handleDragOver}
+            // onDrop={(e) => handleDrop(e, "morning")}
+          >
+            <img src={enAm} alt="mor" className={cs.imgs} />
+            <img src={enpm} alt="mor" className={cs.imgss} />
+            <h1 className={cs.notFound}>NO Item</h1>
+          </div>
+          <div
+            className={`${cs.crewHolderPs} ${cs.crewHolderAdmin} ${cs.crewHolderP}`}
+            // onDragOver={handleDragOver}
+            // onDrop={(e) => handleDrop(e, "night")}
+          >
+            <img src={enAm} alt="mor" className={cs.imgs} />
+            <img src={enpmt} alt="mor" className={cs.imgss} />
+            <h1 className={cs.notFound}>NO Item</h1>
           </div>
         </div>
       </div>
